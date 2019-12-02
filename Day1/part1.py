501ea852425e1e51691f7c890a7a348d7cfaf575
Falco Weihrauch <falco@robaux.de>
2019-12-02 13:54:35 +0100

    clean up

--- Day1/part1.py
+++ Day1/part1.py
@@ -1,3 +1,4 @@
 from math import floor
+
 with open('input.txt', 'r') as file:
-    print(sum([(int(x) // 3) - 2for x in file.read().split('\n')]))
+    print(sum([(int(x) // 3) - 2 for x in file.read().split('\n')]))
