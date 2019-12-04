265891c0cbae0b014f0b519dea943eb8c575249e
Falco Weihrauch <falco@robaux.de>
2019-12-04 10:57:45 +0100

    refactored

--- Day4/app.py
+++ Day4/app.py
@@ -5,13 +5,14 @@ def rule_check(int_val):
     digits = [int(x) for x in str(int_val)]
     adjacent_same = {}
     for idx, val in enumerate(digits):
-        if val not in adjacent_same.keys():
-            adjacent_same[val] = 0
         if idx + 1 < len(digits) and val == digits[idx + 1]:
+            if val not in adjacent_same.keys():
+                adjacent_same[val] = 0
             adjacent_same[val] += 1
         if idx + 1 < len(digits) and val > digits[idx + 1]:
             return False
-    return 1 in list(adjacent_same.values())
+    return list(adjacent_same.values()) or False
 
 
-print(len([x for x in range(*in_range) if rule_check(x)]))
+print(len([x for x in range(*in_range) if rule_check(x)]))  # Part1
+print(len([x for x in range(*in_range) if rule_check(x) and 1 in rule_check(x)]))  # Part2
