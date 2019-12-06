348d64bbd76ce4a284fc6b2f9be9a4691980c61b
Falco Weihrauch <weihrauch@fuzzy.de>
2019-12-06 13:22:56 +0100

    little clean

--- Day6/app.py
+++ Day6/app.py
@@ -1,15 +1,14 @@
 with open('input.txt', 'r') as file:
-    in_dat = {x.split(')')[1]: x.split(')')[0] for x in file.read().split('\n')}
+    in_dat = dict(reversed(line.strip().split(")")) for line in file)
 
 
     def get_path_from_item(item, cf=None):
-        coming_from = cf if cf else item[0]
         out = [item[0]]
         if item[1] in in_dat.keys():
-            out.extend(get_path_from_item((item[1], in_dat[item[1]]), coming_from))
+            out.extend(get_path_from_item((item[1], in_dat[item[1]]), cf if cf else item[0]))
         return out
 
 
     paths = {i[0]: get_path_from_item(i) for i in in_dat.items()}
     print(sum([len(p) for p in paths.values()]))  # Part 1
-    print(len(set(paths["YOU"]).symmetric_difference(set(paths["SAN"]))) - 2)  # Part 2
+    print(len(set(paths["YOU"]) ^ set(paths["SAN"])) - 2)  # Part 2
